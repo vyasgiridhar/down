42c0df799259a26e0490d16c35f3383ee9d33e2c
vyasgiridhar <vyasgiridhar27@gmail.com>
2017-01-14 10:59:09 +0530

    Added single threaded download

--- down.go
+++ down.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 func getHeaders(url string) (header *http.Response) {
@@ -13,10 +16,25 @@ func getHeaders(url string) (header *http.Response) {
 	return
 }
 
+func multiDownload(url string, length int) bool {
+	//x := 4
+	//split := length / x
+	client := new(http.Client)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Range", "0-"+strconv.Itoa(length))
+	data, _ := client.Do(req)
+	dataByte := new(bytes.Buffer)
+	dataByte.ReadFrom(data.Body)
+	ioutil.WriteFile("ba.jpg", dataByte.Bytes(), 0777)
+	return true
+}
+
 func main() {
-	header := getHeaders("https://i.redd.it/uv1x1l3gtg9y.jpg")
+	header := getHeaders("https://dl.dropboxusercontent.com/content_link/b4uW3v4f4qgYyPsKbPDBdKYk529ukI9edxr3IhNQt8GXeyghl3ZPvLcubkfJvtai/file?dl=1")
 	if header.Header.Get("Accept-Ranges") == "bytes" {
 		fmt.Println("Downloading")
-
+		length, _ := strconv.Atoi(header.Header.Get("Content-Length"))
+		multiDownload("https://dl.dropboxusercontent.com/content_link/b4uW3v4f4qgYyPsKbPDBdKYk529ukI9edxr3IhNQt8GXeyghl3ZPvLcubkfJvtai/file?dl=1", length)
 	}
+
 }
